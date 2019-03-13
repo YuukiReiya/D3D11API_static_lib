b529c8939e1f6d7bfad45fa88fa53f6c96f51674
Yuuki-Bamba <yuukibamba@gmail.com>
2019-03-13 11:35:31 +0900

    描画オブジェクトの抽象クラスを追加。 SpriteクラスとShaderManagerクラスの大幅見直し予定。

---
+++ プロジェクト/D3D11API/D3D11API/include/IRender.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "IRender.h"
+
+/*!
+	@brief	usingディレクティブ
+	@using	API
+*/
+using namespace API;
+
+/*!
+	@brief	コンストラクタ
+*/
+IRender::IRender()
+{
+}
+
+/*!
+	@brief	デストラクタ
+*/
+IRender::~IRender()
+{
+}
