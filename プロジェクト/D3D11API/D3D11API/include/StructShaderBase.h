0b63e49a0f391b972987fb2b1ee9ea7172004911
Yuuki Bamba <yuukiBamba@gmail.com>
2019-04-12 21:57:37 +0900

    コンスタントバッファの基底構造体のメンバをW,V,Pの行列に分割

--- プロジェクト/D3D11API/D3D11API/include/StructShaderBase.h
+++ プロジェクト/D3D11API/D3D11API/include/StructShaderBase.h
@@ -17,33 +17,21 @@ namespace D3D11 {
 		/*!
 			@brief	基底頂点構造体
 		*/
-		struct VARTEX_BASE
+		struct BaseVertex
 		{
-			DirectX::XMFLOAT3 m_Pos;/*< 座標 */
+			DirectX::XMFLOAT3 m_Pos;	/*< 座標 */
 		};
 
+#pragma pack(push,16)
 		/*!
 			@brief	シェーダー側に渡すコンスタントバッファの基底構造体
 		*/
-#pragma pack(push,16)
-
-		/*!
-			@brief	packマクロに変更
-		*/
-		struct CONSTANT_BUFFER_BASE
+		struct alignas(16) BaseConstantBuffer
 		{
-			/*!
-				@brief	エイリアステンプレートを用いた別名定義
-			*/
-			
-			template<typename alignType>
-			//using ALIGN16 = __declspec(align(16)) alignType;/*!< 16バイト境界に型を設定するための別名 */
-			using ALIGN16 = alignType;/*!< 16バイト境界に型を設定するための別名 */
-			/*! メンバの行列 */
-			//ALIGN16<DirectX::XMMATRIX>m_WorldMatrix;		/*!< ワールド */
-			//ALIGN16<DirectX::XMMATRIX>m_ProjectionMatrix;	/*!< 射影行列 */
-			//ALIGN16<DirectX::XMMATRIX>m_ViewMatrix;			/*!< ビュー行列 */
-			ALIGN16<DirectX::XMMATRIX>m_WVP;
+			DirectX::XMMATRIX m_WorldMatrix;		/*!< ワールド */
+			DirectX::XMMATRIX m_ProjectionMatrix;	/*!< 射影行列 */
+			DirectX::XMMATRIX m_ViewMatrix;			/*!< ビュー行列 */
+			DirectX::XMMATRIX m_WVP;
 		};
 #pragma pack(pop)
 	};
