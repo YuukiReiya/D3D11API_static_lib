a40dd9b5adc46fa243c803644c8bbd2afa683652
Yuuki Bamba <yuukiBamba@gmail.com>
2019-04-14 17:36:04 +0900

    抽象クラスAbstractShaderのスプライト用派生クラス

---
+++ プロジェクト/D3D11API/D3D11API/include/SpriteShader.cpp
@@ -0,0 +1,175 @@
+#include "stdafx.h"
+#include "SpriteShader.h"
+#include <wrl/client.h>
+#include "MyGame.h"
+#include "Direct3D11.h"
+#include "Sprite.h"
+
+/*!
+	@brief	usingディレクティブ
+	@using	D3D11
+*/
+using namespace D3D11;
+
+/*!
+	@brief	usingディレクティブ
+	@using	D3D11::Graphic
+*/
+using namespace D3D11::Graphic;
+
+/*!
+	@brief	usingディレクティブ
+	@using	Microsoft::WRL
+*/
+using namespace Microsoft::WRL;
+
+/*!
+	@brief	コンストラクタ
+*/
+D3D11::Graphic::SpriteShader::SpriteShader()
+	: AbstractShader()
+	, m_Directory("")
+{
+}
+
+/*!
+	@brief	デストラクタ
+*/
+D3D11::Graphic::SpriteShader::~SpriteShader()
+{
+}
+
+/*!
+	@fn		DynamicSetup
+	@brief	動的コンパイルを利用したセットアップを行う
+	@detail	オーバーライド
+*/
+HRESULT D3D11::Graphic::SpriteShader::DynamicSetup()
+{
+	//	パス
+	auto path = m_Directory + c_HLSL.data();
+
+	//	ブロブ
+	ComPtr<ID3DBlob>pBlob;
+
+	//	ハンドラ
+	HRESULT hr;
+
+
+	//	ファイルを頂点シェーダー用にコンパイル
+	hr = DynamicCompile(path, c_VSEntryName.data(), c_VSProfile.data(), pBlob.GetAddressOf());
+	if (FAILED(hr)) {
+		ErrorLog("\"SpriteShader\" for vertex shader is failed dynamic compilation");
+		return E_FAIL;
+	}
+
+	//	頂点シェーダー作成
+	hr = CreateVertexShader(pBlob.Get(), m_pVertexShader.GetAddressOf());
+	if (FAILED(hr)) {
+		ErrorLog("\"SpriteShader\" is failed create vertex shader");
+		return E_FAIL;
+	}
+
+	//	頂点レイアウト作成
+	hr = CreateInputLayout(pBlob.Get());
+	if (FAILED(hr)) {
+		ErrorLog("\"SpriteShader\" input layout is not create!");
+		return E_FAIL;
+	}
+
+	//	ブロブの開放
+	pBlob->Release();
+	pBlob.Reset();
+
+	//	ファイルをピクセルシェーダー用にコンパイル
+	hr = DynamicCompile(path, c_PSEntryName.data(), c_PSProfile.data(), pBlob.GetAddressOf());
+	if (FAILED(hr)) {
+		ErrorLog("\"SpriteShader\" for pixel shader is failed dynamic compilation");
+		return E_FAIL;
+	}
+	//	ピクセルシェーダー作成
+	hr = CreatePixelShader(pBlob.Get(), m_pPixelShader.GetAddressOf());
+	if (FAILED(hr)) {
+		ErrorLog("\"SpriteShader\" is failed create pixel shader");
+	}
+
+	//	ブロブの開放
+	pBlob->Release();
+	pBlob.Reset();
+
+	//	コンスタントバッファ作成
+	hr = CreateConstantBuffer();
+	if (FAILED(hr)) {
+		std::string error = "\"SpriteShader\" ConstantBuffer is not create!";
+		ErrorLog(error);
+		return E_FAIL;
+	}
+	return S_OK;
+}
+
+/*!
+	@fn			DynamicSetup
+	@brief		ファイルの階層を行い、動的コンパイルを利用したセットアップを行う
+	@detail		オーバーロード
+	@param[in]	ファイルの階層パス
+	@NOTE		シェーダーファイルの配置位置が人によって変わってしまうため、ディレクトリだけでも設定出来るようにする
+*/
+
+HRESULT D3D11::Graphic::SpriteShader::DynamicSetup(std::string& directory)
+{
+	//	ディレクトリ設定
+	SetupDirectory(directory);
+
+	//	動的コンパイル
+	return DynamicSetup();
+}
+
+/*!
+	@fn			SetupDirectory
+	@brief		ファイルを参照するディレクトリを設定
+	@param[in]	参照するディレクトリ
+*/
+void D3D11::Graphic::SpriteShader::SetupDirectory(std::string& path)
+{
+	m_Directory = path;
+}
+
+/*!
+	@fn		CreateConstantBuffer
+	@brief	コンスタントバッファの作成
+	@return	S_OK:成功 E_FAIL:失敗
+*/
+HRESULT D3D11::Graphic::SpriteShader::CreateConstantBuffer()
+{
+	D3D11_BUFFER_DESC cb;
+	SecureZeroMemory(&cb, sizeof(cb));
+	cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cb.ByteWidth = sizeof(SpriteShaderBuffer);
+	cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cb.Usage = D3D11_USAGE_DYNAMIC;
+
+	return Direct3D11::GetInstance().GetDevice()->CreateBuffer(&cb, NULL, m_pConstantBuffer.GetAddressOf());
+}
+
+/*!
+	@fn		CreateInputLayout
+	@brief	頂点レイアウトの作成
+	@return	S_OK:成功 E_FAIL:失敗
+*/
+HRESULT D3D11::Graphic::SpriteShader::CreateInputLayout(ID3DBlob*pBlob)
+{
+	// 頂点インプットレイアウト定義
+	D3D11_INPUT_ELEMENT_DESC layout[] = {
+		{ "POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
+		{ "TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT,0,D3D11_APPEND_ALIGNED_ELEMENT,D3D11_INPUT_PER_VERTEX_DATA,0 },
+	};
+	//	頂点レイアウトの要素数
+	uint32_t numElements = sizeof(layout) / sizeof(*layout);
+	return Direct3D11::GetInstance().GetDevice()->CreateInputLayout(
+		layout,
+		numElements,
+		pBlob->GetBufferPointer(),
+		pBlob->GetBufferSize(),
+		m_pVertexLayout.GetAddressOf()
+	);
+}
---
+++ プロジェクト/D3D11API/D3D11API/include/SpriteShader.h
@@ -0,0 +1,117 @@
+/*
+	@file	SpriteShader
+	@date	2019/04/13
+	@author	番場 宥輝
+	@brief	スプライトのデフォルトシェーダー
+*/
+#pragma once
+#include "AbstractShader.h"
+
+namespace D3D11
+{
+	namespace Graphic {
+
+		class SpriteShader
+			: public AbstractShader
+		{
+		public:
+			/*!
+				@brief	コンストラクタ
+			*/
+			SpriteShader();
+
+			/*!
+				@brief	デストラクタ
+			*/
+			~SpriteShader();
+
+			/*!
+				@fn		Setup
+				@brief	プロコンパイル済みシェーダーファイルを利用してセットアップを行う
+				@detail	オーバーライド
+
+				@TODO	未実装
+			*/
+			HRESULT Setup()override final { return S_OK; };
+
+			/*!
+				@fn		DynamicSetup
+				@brief	動的コンパイルを利用したセットアップを行う
+				@detail	オーバーライド
+			*/
+			HRESULT DynamicSetup()override final;
+
+			/*!
+				@fn			DynamicSetup
+				@brief		ファイルの階層を行い、動的コンパイルを利用したセットアップを行う
+				@detail		オーバーロード
+				@param[in]	ファイルの階層パス
+				@NOTE		シェーダーファイルの配置位置が人によって変わってしまうため、ディレクトリだけでも設定出来るようにする
+			*/
+			HRESULT DynamicSetup(std::string& directory);
+		private:
+			/*!
+				@fn			SetupDirectory
+				@brief		ファイルを参照するディレクトリを設定
+				@param[in]	参照するディレクトリ
+			*/
+			void SetupDirectory(std::string& path);
+
+			/*!
+				@fn		CreateConstantBuffer
+				@brief	コンスタントバッファの作成
+				@return	S_OK:成功 E_FAIL:失敗
+			*/
+			HRESULT CreateConstantBuffer();
+
+			/*!
+				@fn		CreateInputLayout
+				@brief	頂点レイアウトの作成
+				@return	S_OK:成功 E_FAIL:失敗
+			*/
+			HRESULT CreateInputLayout(ID3DBlob*pBlob);
+
+			/*!
+				@var	m_Directory
+				@brief	シェーダーファイルの階層文字列
+			*/
+			std::string m_Directory;
+
+			/*!
+				@var	c_HLSL
+				@brief	HLSLのファイル名
+			*/
+			std::string_view c_HLSL			= "SpriteShader.hlsl";
+
+			/*!
+				@var	c_CSO
+				@brief	CSOのファイル名
+			*/
+			std::string_view c_CSO			= "SpriteShader.cso";
+
+			/*!
+				@var	c_VSEntryName
+				@brief	頂点シェーダーのエントリーポイントの名前
+			*/
+			std::string_view c_VSEntryName	= "VS";
+			
+			/*!
+				@var	c_PSEntryName
+				@brief	ピクセルシェーダーのエントリーポイントの名前
+			*/
+			std::string_view c_PSEntryName	= "PS";
+			
+			/*!
+				@var	c_VSProfile
+				@brief	頂点シェーダーのプロファイル
+			*/
+			std::string_view c_VSProfile	= "vs_5_0";
+
+			/*!
+				@var	c_PSProfile
+				@brief	ピクセルシェーダーのプロファイル
+			*/
+			std::string_view c_PSProfile	= "ps_5_0";
+		};
+	}
+}
