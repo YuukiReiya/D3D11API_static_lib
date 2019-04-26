4d066780c325ab952c79f82aec8494ecbb038a2f
Yuuki Bamba <yuukiBamba@gmail.com>
2019-04-27 08:33:48 +0900

    テスト用のダミープロジェクト更新

--- プロジェクト/FBXHelper TestProject/fbx/main.cpp
+++ プロジェクト/FBXHelper TestProject/fbx/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
-#include <Common.h>
+//#include <Common.h>
 #include <FBXUtility.h>
+//#include <AbstractMesh.h>
+#include <Mesh.h>
+
 
 using namespace FBX;
-
+using namespace Utility;
 int main()
 {
+	//	ÉÅÉÇÉäÉäÅ[ÉNâ”èäì¡íË
+#if defined DEBUG||_DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif // DEBUG||_DEBUG
+
+	FBX::FBXUtility::Setup();
 	FBXUtility::Setup();
 	auto ret = FBXUtility::Load("ao_twinte_chan.fbx");
+	ret = FBXUtility::Load("test.fbx");
 
 	if (ret) {
 		printf("ê¨å˜");
@@ -16,6 +26,20 @@ int main()
 		printf("é∏îs");
 	}
 
+	Mesh*mesh = new Mesh;
+	mesh->Setup();
+	ret=FBXUtility::Load("ao_twinte_chan.fbx", mesh);
+
+	if (ret) {
+		printf("ê¨å˜2");
+	}
+	else {
+		printf("é∏îs2");
+	}
+	FBXUtility::Destroy();
+	FBXUtility::Destroy();
+
+	delete mesh;
 	system("pause");
 	return 0;
 }
