1865e38d6d7f38b094c2a52a0cac460a3bc02e9e
Yuuki Bamba <yuukiBamba@gmail.com>
2019-05-29 11:31:33 +0900

    エントリーポイントと動作シーンの試行錯誤

--- プロジェクト/FBXHelper TestProject/fbx/main.cpp
+++ プロジェクト/FBXHelper TestProject/fbx/main.cpp
@@ -23,7 +23,7 @@ int main()
 	//std::string fbxPath = "Pikachu.fbx";
 
 
-	std::string yfmPath = "test7";
+	std::string yfmPath = "test8";
 
 	IOMesh::Delete("", yfmPath);
 	if (FBX::FBXUtility::Load(fbxPath, mesh))
