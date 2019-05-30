218527fefbde60dcee55571c05818f3236eba8cf
Yuuki Bamba <yuukiBamba@gmail.com>
2019-05-30 13:03:07 +0900

    インデックスを頂点インデックスとUVインデックスを用意、コメントの編集

--- プロジェクト/D3D11API/FBXHelper/source/Mesh.cpp
+++ プロジェクト/D3D11API/FBXHelper/source/Mesh.cpp
@@ -1,16 +1,38 @@
+/*
+	@file	Mesh.cpp
+	@date	2019/05/24
+	@author	番場 宥輝
+	@brief	最低限メッシュを表示する情報を保持したクラス
+*/
 #include "Common.h"
 #include "Mesh.h"
 
+/*!
+	@brief	usingディレクティブ
+	@using	std
+*/
 using namespace std;
+
+/*!
+	@brief	usingディレクティブ
+	@using	Utility
+*/
 using namespace Utility;
 
+/*!
+	@brief	コンストラクタ
+*/
 Mesh::Mesh()
 {
 	vertices.clear();
 	uv.clear();
-	index.clear();
+	vertexIndices.clear();
+	uvIndices.clear();
 }
 
+/*!
+	@brief	デストラクタ
+*/
 Mesh::~Mesh()
 {
 
--- プロジェクト/D3D11API/FBXHelper/source/Mesh.h
+++ プロジェクト/D3D11API/FBXHelper/source/Mesh.h
@@ -2,7 +2,7 @@
 	@file	Mesh.h
 	@date	2019/05/24
 	@author	番場 宥輝
-	@brief	最小限メッシュのクラス
+	@brief	最低限メッシュを表示する情報を保持したクラス
 */
 #pragma once
 #include <vector>
@@ -12,11 +12,38 @@ namespace Utility {
 	class Mesh 
 	{
 	public:
+		/*!
+			@brief	コンストラクタ
+		*/
 		Mesh();
+
+		/*!
+			@brief	デストラクタ
+		*/
 		~Mesh();
 
+		/*!
+			@var	vertices
+			@brief	頂点を格納する可変長配列
+		*/
 		std::vector<Math::FLOAT4>vertices;
+
+		/*!
+			@var	uv
+			@brief	UVを格納する可変長配列
+		*/
 		std::vector<Math::FLOAT2>uv;
-		std::vector<int>index;
+
+		/*!
+			@var	vertexIndices
+			@brief	頂点インデックス
+		*/
+		std::vector<int>vertexIndices;
+
+		/*!
+			@var	uvIndices
+			@brief	UVインデックス
+		*/
+		std::vector<int>uvIndices;
 	};
 }
