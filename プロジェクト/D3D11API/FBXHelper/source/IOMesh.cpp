12c347099380f637d00e2e3f938d72ab1e447c64
Yuuki Bamba <yuukiBamba@gmail.com>
2019-05-30 13:17:13 +0900

    コメント追加

--- プロジェクト/D3D11API/FBXHelper/source/IOMesh.cpp
+++ プロジェクト/D3D11API/FBXHelper/source/IOMesh.cpp
@@ -1,9 +1,25 @@
+/*
+	@file	IOMesh.cpp
+	@date	2019/05/30
+	@author	番場 宥輝
+	@brief	メッシュデータのの入出力関係
+	@detail	シングルトン
+*/
 #include "Common.h"
 #include "IOMesh.h"
 
+/*!
+	@brief	usingディレクティブ
+	@using	std
+*/
 using namespace std;
 
-
+/*!
+	@fn			Delete
+	@brief		yfmファイル削除
+	@param[in]	ファイルのディレクトリ
+	@param[in]	ファイル名(※拡張子除く)
+*/
 void Utility::IOMesh::Delete(std::string directoryPath, std::string fileName)
 {
 	string path = directoryPath + fileName + c_Delimiter.data() + c_Extension.data();
@@ -26,6 +42,14 @@ void Utility::IOMesh::Delete(std::string directoryPath, std::string fileName)
 	remove(path.c_str());
 	cout << "\"" << path << "\" is deleted!" << endl;
 }
+
+/*!
+	@fn			Output
+	@brief		独自形式の出力
+	@param[in]	ファイルのディレクトリ
+	@param[in]	ファイル名(※拡張子除く)
+	@param[in]	メッシュ情報
+*/
 void Utility::IOMesh::Output(std::string directoryPath, std::string fileName, Utility::Mesh mesh)
 {
 	string path = directoryPath + fileName + c_Delimiter.data() + c_Extension.data();
@@ -44,7 +68,7 @@ void Utility::IOMesh::Output(std::string directoryPath, std::string fileName, Ut
 
 	//	頂点インデックス
 	//ofs << "//i" << endl;
-	for (auto it : mesh.index)
+	for (auto it : mesh.vertexIndices)
 	{
 		ofs << it << c_Space;
 	}
@@ -57,45 +81,4 @@ void Utility::IOMesh::Output(std::string directoryPath, std::string fileName, Ut
 		ofs << "{" << it.x << c_Space << it.y << "}";// << endl;
 	}
 
-}
-
-void Utility::IOMesh::OutputDebug(std::string fileName, Mesh mesh, std::vector<int> uvIndex)
-{
-	string path = fileName + c_Delimiter.data() + c_Extension.data();
-	ofstream ofs;
-	ofs.open(path, ios::out);
-
-	//	書き込み
-
-	//頂点
-	const string c_Space = " ";
-	for (auto it : mesh.vertices)
-	{
-		ofs << "{" << it.x << c_Space << it.y << c_Space << it.z << "}";// << endl;
-	}
-	ofs << endl;
-
-	//	頂点インデックス
-	//ofs << "//i" << endl;
-	for (auto it : mesh.index)
-	{
-		ofs << it << c_Space;
-	}
-	ofs << endl;
-
-	//	uv
-	//ofs << "//uv" << endl;
-	for (auto it : mesh.uv)
-	{
-		ofs << "{" << it.x << c_Space << it.y << "}";// << endl;
-	}
-	ofs << endl;
-
-	//	uv index
-	for (auto it : uvIndex)
-	{
-		ofs << it << c_Space;
-	}
-
-	cout << "デバッグアウトプット完了!" << endl;
 }
--- プロジェクト/D3D11API/FBXHelper/source/IOMesh.h
+++ プロジェクト/D3D11API/FBXHelper/source/IOMesh.h
@@ -1,6 +1,6 @@
 /*
 	@file	IOMesh.h
-	@date	2019/04/27
+	@date	2019/05/30
 	@author	番場 宥輝
 	@brief	メッシュデータのの入出力関係
 	@detail	シングルトン
@@ -14,17 +14,42 @@ namespace Utility{
 	class IOMesh
 	{
 	public:
+		/*!
+			@fn			Delete
+			@brief		yfmファイル削除
+			@param[in]	ファイルのディレクトリ
+			@param[in]	ファイル名(※拡張子除く)
+		*/
 		static void Delete(std::string directoryPath, std::string fileName);
+
+		/*!
+			@fn			Output
+			@brief		独自形式の出力
+			@param[in]	ファイルのディレクトリ
+			@param[in]	ファイル名(※拡張子除く)
+			@param[in]	メッシュ情報
+		*/
 		static void Output(std::string directoryPath, std::string fileName, Mesh mesh);
-		static void OutputDebug(std::string fileName, Mesh mesh, std::vector<int>uvIndex);
 	private:
+		/*!
+			@brief	コンストラクタ削除
+		*/
 		IOMesh() = delete;
+
+		/*!
+			@brief	デストラクタ削除
+		*/
 		~IOMesh() = delete;
 
+		/*!
+			@var	c_Delimiter
+			@brief	拡張子の区切り文字
+		*/
 		static constexpr std::string_view c_Delimiter = ".";
 
 		/*!
-			@brief	y(uuki)f(ormat)m(esh)
+			@brief	独自形式の拡張子
+			@detail	y(uuki)f(ormat)m(esh)
 		*/
 		static constexpr std::string_view c_Extension = "yfm";
 	};
