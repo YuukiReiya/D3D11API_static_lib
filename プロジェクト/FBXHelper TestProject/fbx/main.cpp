77382b09beaf2d4f05888c0a287fb35f6cb20362
Yuuki Bamba <yuukiBamba@gmail.com>
2019-07-05 10:43:35 +0900

    エントリーポイントとフィルタ情報更新

--- プロジェクト/FBXHelper TestProject/fbx/main.cpp
+++ プロジェクト/FBXHelper TestProject/fbx/main.cpp
@@ -16,17 +16,17 @@ int main()
 	//	読み込みファイル
 	//std::string fbxPath = "SD_unitychan_humanoid.fbx";
 	//std::string fbxPath = "ao_twinte_chan.fbx"; 
-	std::string fbxPath = "Kouhai_chan.fbx";
+	//std::string fbxPath = "Kouhai_chan.fbx";
 	//std::string fbxPath = "JNT_00100_anathema_H.fbx";
 	//std::string fbxPath = "Pikachu.fbx";
-	//std::string fbxPath = "humanoid.fbx";
+	std::string fbxPath = "humanoid.fbx";
 
 
 	//	書き出しファイル
 	//std::string yfmPath = "sd-unity";
-	//std::string yfmPath = "humanoid";
+	std::string yfmPath = "humanoid";
 	//std::string yfmPath = "twin";
-	std::string yfmPath = "test00";
+	//std::string yfmPath = "test00";
 
 	//	コンバート
 	Converter::FBXConverter::Setup();
