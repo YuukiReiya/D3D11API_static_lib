a6f12124b7fc83cb832519e7085c87bbd20ed4c5
Yuuki Bamba <yuukiBamba@gmail.com>
2019-08-27 21:51:46 +0900

    コンバーターをファイル指定できるように改良。
    プロジェクトファイルはプリプロセッサ定義の”FBXSDK_SHARED”を削除。
    ↑ビルドエラーの原因。

--- プロジェクト/FBXHelper TestProject/fbx/main.cpp
+++ プロジェクト/FBXHelper TestProject/fbx/main.cpp
@@ -3,8 +3,13 @@
 #include <Mesh.h>
 #include <IOMesh.h>
 #include <WinConsoleExpansion.h>
+#include <string>
 using namespace Utility;
 using namespace Converter;
+using namespace std;
+using namespace ConsoleExpansion;
+
+using wic = ConsoleExpansion::WinConsoleExpansion;
 
 int main()
 {
@@ -18,12 +23,91 @@ int main()
 	/////////////////////////////////////////////////
 	
 	//	読み込む.fbxファイルのパスを指定します。
-	std::string fbxPath = "";
-	fbxPath = "Spider_3.fbx";
+	string fbxPath = "";
+	do
+	{
+		wic::SetColor(Cyan);
+		cout << "FBXファイルのパスを指定してください。" << endl;
+		wic::SetColor(White);
+		getline(cin, fbxPath);
+		wic::SetColor(Cyan);
+
+		//	FBXファイルが指定されているか確認
+		try
+		{
+			auto delimiterOffset = fbxPath.rfind(".");
+			if (delimiterOffset == string::npos) { throw -1; }
+			auto extension = fbxPath.substr(delimiterOffset);
+			if (extension != ".fbx") { throw -1; }
+		}
+		//	FBXファイルが指定されていないので、再入力を促す。
+		catch (...)
+		{
+			wic::SetColor(Red);
+			cout << ".fbxファイルが指定されていません!" << endl
+				<< "再入力してください。" << endl;
+			continue;
+		}
+
+		//	入力の確定確認
+		string ret;
+		cout << "\"" << fbxPath << "\"" << "を読み込みます。" << endl
+			<< "よろしいですか?<y/n>" << endl;
+		getline(cin, ret);
+
+		//	書き出しファイルの指定に移る
+		if (ret == "y") 
+		{ 
+			break;
+		}
+		//	アプリケーション終了
+		else if(ret == "n")
+		{
+			wic::SetColor(Purple);
+			cout << "コンバーターを終了します。" << endl;
+			wic::SetColor(White);
+			system("pause");
+			return -1;
+		}
+
+		//	コンソールを綺麗にする
+		system("cls");
+	} while (true);
 
 	//	書き出すファイルの名前を指定します。
 	std::string yfmPath = "";
-	yfmPath = "spider";
+	do
+	{
+		wic::SetColor(Cyan);
+		cout << "書き出し先のパス及びフォルダ名を指定してください。" << endl;
+		wic::SetColor(White);
+		getline(cin, yfmPath);
+		wic::SetColor(Cyan);
+
+		string ret;
+		cout << "\"" << yfmPath << "\"" << "に書き出します。" << endl
+			<< "よろしいですか?<y/n>" << endl;
+		getline(cin, ret);
+
+		//	入力の確定確認
+		if (ret == "y")
+		{
+			break;
+		}
+		//	アプリケーションの終了
+		else if (ret == "n")
+		{
+			wic::SetColor(Purple);
+			cout << "コンバーターを終了します。" << endl;
+			wic::SetColor(White);
+			system("pause");
+			return -1;
+		}
+
+		//	コンソールをクリア
+		system("cls");
+	} while (true);
+
 
 	/////////////////////////////////////////////////
 	//	コンバート
